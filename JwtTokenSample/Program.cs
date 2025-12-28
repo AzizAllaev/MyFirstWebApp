9dc3b48ae5536eabb5178bacaa04df70647af294
azizallaev78@gmail.com <azizallaev78@gmail.com>
2025-12-28 18:46:57 +0500

    File structure cahnged

--- JwtTokenSample/Program.cs
+++ JwtTokenSample/Program.cs
@@ -1,36 +1,60 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using JwtTokenSample.Models;
+using JwtTokenSample.Services;
+
 namespace JwtTokenSample
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
-			builder.Services.AddSwaggerGen();
+			builder.Services.AddDbContext<NorthwindContext>(options =>
+	            options.UseSqlServer(builder.Configuration.GetConnectionString(@"Server=.\SQLEXPRESS;Database=Northwind;Trusted_Connection=True")));
+			builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	            .AddJwtBearer(options =>
+	            {
+	            	options.TokenValidationParameters = new TokenValidationParameters
+	            	{
+		            	ValidateIssuer = true,
+		            	ValidateAudience = true,
+			            ValidateLifetime = true,
+			            ValidateIssuerSigningKey = true,
+		            	ValidIssuer = "yourdomain.com",
+		            	ValidAudience = "yourdomain.com",
+		            	IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("secretkeysecretkeysecretkeysecretkeysecretkey"))
+		            };
+	            });
 
-			var app = builder.Build();
+            builder.Services.AddAuthorization();
+            builder.Services.AddScoped<ITokenGenerator, TokenGenerator>();
+            // Add services to the container.
+			builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
+            var app = builder.Build();
 
-			app.UseHttpsRedirection();
-			app.UseRouting();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
-			app.UseAuthorization();
+            app.UseHttpsRedirection();
 
-			app.MapStaticAssets();
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}")
-				.WithStaticAssets();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-			app.Run();
-		}
-	}
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
